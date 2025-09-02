2a0db3e71a6971f91768152135e96b2c186cdc7d
Mohit Attri <attrimohit6846@gmail.com>
2025-09-02 16:26:15 +0530

    A

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const dbgr = require('debug')('development:mongoose');
 
-mongoose.connect(`${config.get("MONGODB_URI")}/ecommerce`)
+mongoose.connect(`${config.get("MONGODB_URI") || 'mongodb://127.0.0.1:27017'}/ecommerce`)
 .then(function(){
     dbgr("Connected to MongoDB");
 })
